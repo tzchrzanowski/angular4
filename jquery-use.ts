d8bc95a014c8bb5c0940c6ac623a2fc7b6f1c35d
Tom <tzchrzanowski@gmail.com>
2017-06-17 09:30:55 +0200

    Create jquery-use.ts

---
+++ jquery-use.ts
@@ -0,0 +1,18 @@
+// import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+// @Component({
+//   selector: 'app-hacf',
+//   templateUrl: './hacf.component.html',
+//   styleUrls: ['./hacf.component.css']
+// })
+// export class HacfComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+       $(document).ready(function(){
+         $('.scrollspy').scrollSpy();
+       });
+//   }
+// }
